4440762e4da9d4ae9b1ba8f73f951f8322d2ffe3
Yu Jin Choe <forworkchoe@gmail.com>
2025-07-03 15:14:26 +0900

    (0.0.24) NonDirectiveClientComponent도 에러가 안나길래 use client 있던 컴포넌트 삭제

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export {default as SpecialButton} from './components/SpecialButton';
 export {default as AnotherButton} from './components/AnotherButton';
 export {default as ThirdButton} from './components/ThirdButton';
-export {default as DirectiveClientComponent} from './components/DirectiveClientComponent';
 export {default as NonDirectiveClientComponent} from './components/NonDirectiveClientComponent';
 export {add} from './utils/calculator.ts';
